e1fdb801fcc05e184fdcd0d05f16c78b5c2331a6
ikoma44 <orechu.chu.chu@gmail.com>
2021-11-26 14:43:22 +0000

    トップ作成,新規投稿途中

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,27 @@
 class Post < ApplicationRecord
   has_one :spot
-  
+
+  enum fishing_spot_id: {
+    海: 0,
+    湖: 1,
+    川: 2,
+    その他: 3,
+  }
+
+  enum spot_type: {
+    河川: 0,
+    湖: 1,
+    池: 2,
+    港: 3,
+    堤防: 4,
+    海岸: 5,
+    磯: 6,
+    沖: 7,
+    海上釣り掘: 8,
+    管理釣り場: 9,
+    カセ: 10,
+    イカダ: 11,
+    その他: 12,
+  }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   get '/' => 'homes#top'
-  
+
+  namespace :users do
+    resources :posts do
+      collection do
+        delete 'destroy_all'
+      end
+    end
+  end
 end
